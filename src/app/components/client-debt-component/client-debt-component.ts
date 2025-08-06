6b267a62fe13ce8d74fa54c1276b404286679f5c
Michal Boczula <michal.boczula0101@gmail.com>
2025-08-06 22:01:45 +0200

    mocked client and changed layout

--- src/app/components/client-debt-component/client-debt-component.ts
+++ src/app/components/client-debt-component/client-debt-component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ClientPaymentComponent } from "../client-payment-component/client-payment-component";
+import { Debt } from '../../models/debt.model';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-client-debt-component',
-  imports: [ClientPaymentComponent],
+  imports: [ClientPaymentComponent, CommonModule, MatCardModule],
+  providers: [CurrencyPipe, DatePipe],
   templateUrl: './client-debt-component.html',
-  styleUrl: './client-debt-component.css'
+  styleUrl: './client-debt-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ClientDebtComponent {
-
+  @Input() debts: Debt[] = [];
 }
--- src/app/components/client-form-component/client-form-component.ts
+++ src/app/components/client-form-component/client-form-component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -7,6 +7,7 @@ import { ClientService } from '../client.service';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ClientDebtComponent } from "../client-debt-component/client-debt-component";
+import { ClientInstance } from '../../models/client-instance.model';
 
 @Component({
   selector: 'app-client-form-component',
@@ -19,12 +20,60 @@ import { ClientDebtComponent } from "../client-debt-component/client-debt-compon
 export class ClientFormComponent {
   private readonly clientService = inject(ClientService);
 
-  client = this.clientService.client;
+  mockedClient: ClientInstance = {
+    id: 'e54fe002-9f23-4a39-b85a-05aea5a4fb96',
+    firstName: 'John',
+    lastName: 'Doe',
+    bankAccountNumber: '123-456',
+    debts: [
+      {
+        id: '8e73adba-e2c1-44d0-85bd-24dbfa4b8b31',
+        amount: 1000,
+        dueDate: '2025-08-31T22:00:00.000Z',
+        payments: [
+          {
+            id: 'b9280a70-0f06-401f-833d-1b8cb378e263',
+            amount: 300,
+            date: '2025-07-31T22:00:00.000Z',
+            note: 'Initial payment'
+          },
+          {
+            id: 'b9280a70-0f06-401f-833d-1b8cb378e263',
+            amount: 300,
+            date: '2025-08-31T22:00:00.000Z',
+            note: 'Initial payment'
+          }
+        ]
+      },
+      {
+        id: '2e73adba-e2c1-44d0-85bd-24dbfa4b8b31',
+        amount: 2000,
+        dueDate: '2025-08-31T22:00:00.000Z',
+        payments: [
+          {
+            id: 'b9280a70-0f06-401f-833d-1b8cb378e263',
+            amount: 500,
+            date: '2025-07-31T22:00:00.000Z',
+            note: 'Initial payment'
+          },
+          {
+            id: 'b9280a70-0f06-401f-833d-1b8cb378e263',
+            amount: 500,
+            date: '2025-08-31T22:00:00.000Z',
+            note: 'Initial payment'
+          }
+        ]
+      }
+    ]
+  };
+
+  // client = this.clientService.client;
+  client = signal<ClientInstance>(this.mockedClient);
   loading = this.clientService.loading;
   error = this.clientService.error;
 
   constructor() {
-    this.clientService.loadClient('ee7223ba-6c39-4a63-ab58-b04ccc3dca8c');
+    // this.clientService.loadClient('ee7223ba-6c39-4a63-ab58-b04ccc3dca8c');
   }
 }
 
